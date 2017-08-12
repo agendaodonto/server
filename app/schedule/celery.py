5796beabf42b04baba181ba0e28a53441330510d
André Roggeri Campos <a.roggeri.c@gmail.com>
2017-08-12 13:54:50 -0300

    Tune Celery for CloudAMQP (Closes #34)

--- app/schedule/celery.py
+++ app/schedule/celery.py
@@ -8,7 +8,10 @@ celery_app.conf.update(
     broker_url=settings.CELERY_BROKER_URL,
     result_backend=settings.CELERY_RESULT_BACKEND,
     task_always_eager=settings.CELERY_ALWAYS_EAGER,
-    task_eager_propagates=settings.CELERY_EAGER_PROPAGATE
+    task_eager_propagates=settings.CELERY_EAGER_PROPAGATE,
+    broker_heartbeat=settings.CELERY_BROKER_HEARTBEAT,
+    worker_send_task_events=settings.CELERY_SEND_EVENTS,
+    event_queue_expires=settings.CELERY_EVENT_QUEUE_EXPIRES
 )
 
 celery_app.autodiscover_tasks()
