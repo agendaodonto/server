9cea4111a4820247616c289d34daead4efaaefc9
André Roggeri Campos <a.roggeri.c@gmail.com>
2017-09-12 22:59:35 -0300

    Fix wrong ETA (Closes #61)

--- app/schedule/celery.py
+++ app/schedule/celery.py
@@ -4,7 +4,7 @@ from django.conf import settings
 celery_app = Celery('messenger')
 
 celery_app.conf.update(
-    timezone=settings.TIME_ZONE,
+    # timezone=settings.TIME_ZONE,
     broker_url=settings.CELERY_BROKER_URL,
     result_backend=settings.CELERY_RESULT_BACKEND,
     task_always_eager=settings.CELERY_ALWAYS_EAGER,
