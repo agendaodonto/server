2a654f530283f12f78650904382e43fa5a9cf45a
André Roggeri Campos <a.roggeri.c@gmail.com>
2018-01-23 00:28:14 -0200

    fix: Deploy script with correct parameters

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,19 +5,14 @@ if [[ ${TRAVIS_TEST_RESULT=0} == 1 ]]; then
   exit 1;
 fi
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-  echo "Skipping pull request !!"
-  exit 0;
-fi
-
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 .travis/deploy.key # this key should have push access
 ssh-add .travis/deploy.key
-ssh-keyscan <your dokku host> >> ~/.ssh/known_hosts
+ssh-keyscan deploy.agendaodonto.com  >> ~/.ssh/known_hosts
 git config --global push.default simple
 
 if [[ $TRAVIS_BRANCH == "master" ]]; then
-  git remote add deploy.agendaodonto.com:backend
+  git remote add deploy dokku@deploy.agendaodonto.com:backend
   git push deploy master:master
   echo "Deploying to Production"
 else
@@ -25,10 +20,9 @@ else
 fi
 
 if [[ $TRAVIS_BRANCH == "develop" ]]; then
-  git remote add deploy.agendaodonto.com:backend-staging
+  git remote add deploy dokku@deploy.agendaodonto.com:backend-staging
   git push deploy develop:master
   echo "Deploying to Staging"
 else
   echo "Skipping deploy to staging. (Not on develop branch)"
 fi
-
