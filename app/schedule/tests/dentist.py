6337d24bc263303e6f3072b51f044cacd5553755
André Roggeri Campos <a.roggeri.c@gmail.com>
2018-02-05 22:40:17 -0200

    chore: Remove pyJWT dependency (Closes #58)

--- app/schedule/tests/dentist.py
+++ app/schedule/tests/dentist.py
@@ -1,8 +1,8 @@
 import json
 
 from django.urls import reverse
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
-from rest_framework_jwt.settings import api_settings
 
 from app.schedule.models import Dentist
 
@@ -11,15 +11,11 @@ class DentistAPITest(APITestCase):
     def setUp(self):
         self.dentist = Dentist.objects.create_user('John', 'Snow', 'john@snow.com', 'M', '1234', 'SP', 'john')
         self.extra_dentist = Dentist.objects.create_user('Maria', 'Dolores', 'maria@d.com', 'F', '5555', 'RJ', 'maria')
-        self.api_authentication()
+        self.authenticate()
 
-    def api_authentication(self):
-        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-
-        payload = jwt_payload_handler(self.dentist)
-        token = jwt_encode_handler(payload)
-        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
+    def authenticate(self):
+        token = Token.objects.create(user=self.dentist)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
 
     def test_get_dentist(self):
         url = reverse('dentists') + '?cro=234'
