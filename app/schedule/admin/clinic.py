8027f9190da5c4939461244a92cf49921ac36ccf
André Roggeri Campos <a.roggeri.c@gmail.com>
2018-02-05 23:01:26 -0200

    chore: Remove the admin and its routes

--- app/schedule/admin/clinic.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from app.schedule.models.clinic import Clinic
-
-admin.site.register(Clinic)
--- app/schedule/admin/schedule.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from app.schedule.models import Schedule
-
-admin.site.register(Schedule)
