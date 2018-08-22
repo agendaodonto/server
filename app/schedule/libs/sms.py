b4fa0a105e253f522ab05b79b015ad0e0d6c7b1e
André Roggeri Campos <a.roggeri.c@gmail.com>
2018-08-21 23:59:00 -0300

    feat: Update the SMSGateway client (Closes #186)

--- app/schedule/libs/sms.py
+++ app/schedule/libs/sms.py
@@ -5,46 +5,24 @@ import time
 from django.conf import settings
 from smsgateway import SMSGateway, Message
 
-from app.schedule.utils.utils import parse_js_date
-
 
 class SMS:
     def __init__(self, token: str):
         self.client = SMSGateway(token)
         self.device = None
 
-    def get_best_device(self) -> int:
-        logger = logging.getLogger(__name__)
-        best_device = None
-        devices = self.client.search_devices()['results']
+    def get_latest_device(self) -> int:
+        device_filter = {
+            'order_by': [{'field': 'id', 'direction': 'desc'}],
+            'limit': 1
+        }
+        devices = self.client.search_devices(device_filter)['results']
 
-        for device in devices:
-            try:
-                self.check_device_suitability(device)
-                if not best_device:
-                    best_device = device
-                else:
-                    best_last_seen = parse_js_date(best_device['attributes']['last_seen'])
-                    current_last_seen = parse_js_date(device['attributes']['last_seen'])
-                    if best_last_seen > current_last_seen:
-                        best_device = device
-            except NotSuitableDevice as e:
-                logger.warning("Device {} not suitable because {}".format(device['id'], e))
-        else:
-            logger.error('No device found')
+        if len(devices) <= 0:
+            raise DeviceNotFoundError()
 
-        if not best_device:
-            raise DeviceNotFoundError('No Device found.')
-        logger.info("""
-                    Best device found: {}
-                    """.format(best_device['id']))
-        self.device = best_device['id']
+        self.device = devices[0]['id']
         return self.device
-
-    def check_device_suitability(self, device):
-        last_seen = parse_js_date(device['attributes']['last_seen'])
-        if int(time.time()) - last_seen.timestamp() > settings.SMS_MIN_MISSING_TIME:
-            raise NotSuitableDevice('Missing for more than {} minutes'.format(settings.SMS_MIN_MISSING_TIME))
 
     def send_message(self, to: str, message: str) -> bool:
         message = Message(to, message, self.device)
@@ -83,5 +61,5 @@ class DummySMS:
     def send_message(self, to, message):
         return True
 
-    def get_best_device(self):
-        return True
+    def get_latest_device(self):
+        return 1
