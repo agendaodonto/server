2a3fcaab134f1d77ffd31fccf903ebf2f9db570b
André Roggeri Campos <a.roggeri.c@gmail.com>
2018-09-27 23:55:11 -0300

    Removes the messaging logic

--- app/schedule/tasks.py
+++ app/schedule/tasks.py
@@ -1,16 +1,16 @@
 from django.conf import settings
 
 from app.schedule.celery import celery_app
-from app.schedule.libs.sms import DeviceNotFoundError
 
 
 @celery_app.task(bind=True)
 def send_message(self, to, message):
-    messenger = settings.APP_MESSENGER_CLASS
-    token = settings.SMS_GATEWAY_TOKEN
-    try:
-        messenger = messenger(token)
-        messenger.get_latest_device()
-        return messenger.send_message(to, message)
-    except DeviceNotFoundError as e:
-        self.retry(exc=e, max_retries=settings.CELERY_TASK_MAX_RETRY, countdown=60 * 5)
+    # messenger = settings.APP_MESSENGER_CLASS
+    # token = settings.SMS_GATEWAY_TOKEN
+    # try:
+        # messenger = messenger(token)
+        # messenger.get_latest_device()
+        # return messenger.send_message(to, message)
+    # except DeviceNotFoundError as e:
+    #     self.retry(exc=e, max_retries=settings.CELERY_TASK_MAX_RETRY, countdown=60 * 5)
+    pass
