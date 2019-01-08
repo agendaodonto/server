13141082b6e0b818ac29848d563a851e13897b8a
André Roggeri Campos <a.roggeri.c@gmail.com>
2019-01-08 02:08:57 -0200

    Add first docker example !

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -1,15 +1,12 @@
 import datetime
 
+import dj_database_url
+
 from .default import *
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config()}
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -17,8 +14,9 @@ JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=10)
 }
 
-CELERY_ALWAYS_EAGER = True
-CELERY_EAGER_PROPAGATE = True
+# Celery Settings
+CELERY_BROKER_URL = os.environ['RABBITMQ_URL']
+CELERY_BROKER_HEARTBEAT = None
 
 MESSAGE_ETA = {'hour': 0, 'minute': 0}
 MESSAGE_EXPIRES = {'hour': 23, 'minute': 59}
