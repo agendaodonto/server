6b96de465d17e8822b9ec094986d5df9fa6fb8bc
André Roggeri Campos <a.roggeri.c@gmail.com>
2020-03-27 21:42:21 -0300

    test: Split test for each endpoint

--- app/finance/tests/transaction_type_list.py
+++ app/finance/tests/transaction_type_list.py
@@ -7,17 +7,11 @@ from app.finance.serializers.transaction_type import TransactionTypeSerializer
 from app.schedule.models import Dentist, Clinic
 
 
-class TransactionTypeAPITest(APITestCase):
+class TransactionTypeListAPITest(APITestCase):
     def setUp(self):
         self.dentist = Dentist.objects.create_user('John', 'Snow', 'john@snow.com', 'M', '1234', 'SP', 'john')
         self.clinic = Clinic.objects.create(
             name='Test Clinic',
-            owner=self.dentist,
-            message='',
-            time_delta=0
-        )
-        self.clinic2 = Clinic.objects.create(
-            name='Test Clinic2',
             owner=self.dentist,
             message='',
             time_delta=0
@@ -60,38 +54,6 @@ class TransactionTypeAPITest(APITestCase):
         self.assertEqual(transaction_types.count(), 1)
         self.assertEqual(self.serializer.to_representation(transaction_types[0]), content)
 
-    def test_should_update_transaction_type(self):
-        # Arrange
-        avail_transaction = TransactionType.objects.create(code=1234, clinic=self.clinic, label='Some label here')
-        url = reverse('transaction-type-detail', kwargs={'pk': avail_transaction.id})
-
-        # Act
-        content = {
-            'code': 5599,
-            'clinic': self.clinic2.id,
-            'label': 'Some !!! Label1234'
-        }
-        req = self.client.put(url, content)
-
-        # Assert
-        transaction_types = TransactionType.objects.all()
-        self.assertEqual(req.status_code, 200)
-        self.assertEqual(transaction_types.count(), 1)
-        self.assertEqual(content, self.serializer.to_representation(transaction_types[0]))
-
-    def test_should_delete_transaction_type(self):
-        # Arrange
-        avail_transaction = TransactionType.objects.create(code=1234, clinic=self.clinic, label='Some label here')
-        url = reverse('transaction-type-detail', kwargs={'pk': avail_transaction.id})
-
-        # Act
-        req = self.client.delete(url)
-
-        # Assert
-        transaction_types = TransactionType.objects.all()
-        self.assertEqual(req.status_code, 204)
-        self.assertEqual(transaction_types.count(), 0)
-
     def test_should_ensure_code_is_unique(self):
         # Arrange
         url = reverse('transaction-types')
@@ -115,12 +77,18 @@ class TransactionTypeAPITest(APITestCase):
     def test_should_only_return_transactions_type_for_clinics_owned_by_user(self):
         # Arrange
         other_dentist = Dentist.objects.create_user('John', 'Snow', 'aria@stark.com', 'F', '5599', 'SP', 'john')
-        self.clinic2.owner = other_dentist
-        self.clinic2.save()
+        clinic2 = Clinic.objects.create(
+            name='Test Clinic2',
+            owner=self.dentist,
+            message='',
+            time_delta=0
+        )
+        clinic2.owner = other_dentist
+        clinic2.save()
 
         url = reverse('transaction-types')
         avail_transaction1 = TransactionType.objects.create(code=1234, clinic=self.clinic, label='Some label here')
-        avail_transaction2 = TransactionType.objects.create(code=5555, clinic=self.clinic2, label='Some other here')
+        avail_transaction2 = TransactionType.objects.create(code=5555, clinic=clinic2, label='Some other here')
 
         # Act
         req = self.client.get(url)
